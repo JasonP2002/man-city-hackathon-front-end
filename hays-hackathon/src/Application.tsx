f9be7e349fc1091d547e5afec8e37c289c238e06
Vinicius Ribeiro <vayne99@gmail.com>
2023-04-10 09:35:46 -0300

    feat: list all players and specific player service

--- hays-hackathon/src/Application.tsx
+++ hays-hackathon/src/Application.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./components/pages/Home";
 import SelectionPage from "./components/pages/SelectionPage";
 import ErrorPage from "./components/pages/errorPage";
 import MatchPage from "./components/pages/MatchPage";
 import "./Application.css";
+import { player } from "./services";
 
 export interface IApplicationProps {}
 
 const Application: React.FunctionComponent<IApplicationProps> = (props) => {
+  const { getAllPlayers, getPlayerById } = player();
+
+  useEffect(() => {
+    requests();
+    async function requests() {
+      console.log("all", await getAllPlayers());
+      console.log("id", await getPlayerById(4649));
+    }
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
