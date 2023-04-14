5ea5fe7da861e94cf6ac94ac5d7f74c9b7d5f86f
jnp000 <jnp000@student.bham.ac.uk>
2023-04-14 20:26:15 +0100

    moved components to match page

--- hays-hackathon/src/components/pages/Home.tsx
+++ hays-hackathon/src/components/pages/Home.tsx
@@ -1,24 +1,12 @@
 import React from "react";
 import Layout from "../layout/layout";
 
-import Field from '../home/Field';
-import Bench from '../home/Bench';
-import Available from '../home/Available';
-
-import { DndContext } from '@dnd-kit/core';
-
 export interface IHomePageProps {}
 
 const HomePage: React.FunctionComponent<IHomePageProps> = (props) => {
   return (
     <Layout>
-      {/*Must wrap all drag-and-drop components in a DndContext*/}
-      <DndContext>
         <h1>Home</h1>
-        <Field />
-        <Bench />
-        <Available />
-      </DndContext>
     </Layout>
   );
 };
--- hays-hackathon/src/components/pages/MatchPage.tsx
+++ hays-hackathon/src/components/pages/MatchPage.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Layout from "../layout/layout";
 
+import Field from '../home/Field';
+import Bench from '../home/Bench';
+import Available from '../home/Available';
+
+import { DndContext } from '@dnd-kit/core';
+
 export interface IMatchPageProps {}
 
 const MatchPage: React.FunctionComponent<IMatchPageProps> = (props) => {
   return (
     <Layout>
-      <div>match</div>
+      {/*Must wrap all drag-and-drop components in a DndContext*/}
+      <DndContext>
+        <h1>Match</h1>
+        <Field />
+        <Bench />
+        <Available />
+      </DndContext>
     </Layout>
   );
 };
