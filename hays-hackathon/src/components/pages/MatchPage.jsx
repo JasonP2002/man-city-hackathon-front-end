786df9e714e0d7924ae0ade90cf90401cc471175
jnp000 <jnp000@student.bham.ac.uk>
2023-04-14 23:37:33 +0100

    drag-and-drop with multiple draggables

--- hays-hackathon/src/components/pages/MatchPage.jsx
+++ hays-hackathon/src/components/pages/MatchPage.jsx
@@ -9,18 +9,37 @@ import { DndContext } from '@dnd-kit/core';
 
 import Player from '../matchpage/Player';
 
+const num_players = 4;
+
+const generatePlayers = () => {
+  let players = []
+  let id=''
+  for (let i = 0; i < num_players; i++) {
+    id = 'player-draggable:'+i.toString()
+    players.push(<Player key={id} id={id}>Player {i+1}</Player>)
+  }
+  return players;
+};
+
+const generateDropZones = () => {
+  let dropZones = []
+  for (let i = 0; i < num_players; i++) {
+    dropZones.push('avail-droppable')
+  }
+  return dropZones;
+}
 
 const MatchPage = (props) => {
-  const [parent, setParent] = useState(null);
-  const testPlayer = (
-    <Player id="player-draggable">
-      <p>Player</p>
-    </Player>
-  )
+  const [players, setPlayers] = useState(generatePlayers());
+  const [dropZones, setDropZones] = useState(generateDropZones());
 
   function handleDragEnd(event) {
-    const {over} = event;
-    setParent(over ? over.id : null);
+    const {over, active} = event;
+    if (over && active) { //If draggable dropped into droppable
+      const index = parseInt((active.id).split(':')[1]); //Get id of draggable
+      //Change dropzone for dropped draggable
+      setDropZones(dropZones.slice(0, index).concat([over.id].concat(dropZones.slice(index+1, dropZones.length))));
+    }
   }
 
   return (
@@ -32,17 +51,17 @@ const MatchPage = (props) => {
 
           <Field key='field-droppable' id='field-droppable'>
             <h2>Field</h2>
-            {parent === 'field-droppable' ? testPlayer : <></>}
+            {players.filter((_, i) => dropZones[i] === 'field-droppable')}
           </Field>
 
           <Bench key='bench-droppable' id='bench-droppable'>
             <h2>Bench</h2>
-            {parent === 'bench-droppable' ? testPlayer : <></>}
+            {players.filter((_, i) => dropZones[i] === 'bench-droppable')}
           </Bench>
 
           <Available key='avail-droppable' id='avail-droppable'>
             <h2>Players</h2>
-            {parent === null || parent === 'avail-droppable' ? testPlayer : null}
+            {players.filter((_, i) => dropZones[i] === 'avail-droppable')}
           </Available>
 
         </DndContext>
