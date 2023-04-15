4988a2a8c1003385fe0f8b295ddccd4c277a2f6f
DR3Y13 <andreyaem.aem@gmail.com>
2023-04-15 02:05:56 +0100

    restructuring

--- hays-hackathon/src/components/pages/SelectionPage.tsx
+++ hays-hackathon/src/components/pages/SelectionPage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useLocation } from "react-router";
 import Layout from "../layout/layout";
 
 export interface ISelectionPageProps {}
-
 const SelectionPage: React.FunctionComponent<ISelectionPageProps> = (props) => {
+  const { state } = useLocation();
+  console.log(state);
   return (
     <Layout>
       <div>selection</div>
