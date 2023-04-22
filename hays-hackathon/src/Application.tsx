86a1c2396b967560a123e44ca48f388f8106f5cb
DR3Y13 <andreyaem.aem@gmail.com>
2023-04-22 14:22:25 +0100

    restructuring

--- hays-hackathon/src/Application.tsx
+++ hays-hackathon/src/Application.tsx
@@ -1,11 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./components/pages/Home";
-import SelectionPage from "./components/pages/SelectionPage";
-import ErrorPage from "./components/pages/errorPage"
-import MatchPage from "./components/pages/MatchPage";
+import ErrorPage from "./components/pages/errorPage";
 import "./Application.css";
 import ArchivePage from "./components/pages/archivePage";
 import React from "react";
+import MatchPage from "./components/pages/MatchPage";
 
 export interface IApplicationProps {}
 
@@ -14,7 +13,6 @@ const Application: React.FunctionComponent<IApplicationProps> = (props) => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="selection" element={<SelectionPage />} />
         <Route path="match" element={<MatchPage />} />
         <Route path="archive" element={<ArchivePage />} />
         <Route path="*" element={<ErrorPage />} />
--- hays-hackathon/src/components/pages/archivePage.tsx
+++ hays-hackathon/src/components/pages/archivePage.tsx
@@ -1,25 +1,69 @@
 import * as React from "react";
 import Layout from "../layout/layout";
 import axios from "axios";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import {
+  DataGrid,
+  GridColDef,
+  GridToolbarContainer,
+  GridToolbarExport,
+} from "@mui/x-data-grid";
 import { useState } from "react";
 import { Box } from "@mui/system";
 
 export interface IArchivePageProps {}
 
 const ArchivePage: React.FunctionComponent<IArchivePageProps> = (props) => {
+  function CustomToolbar() {
+    return (
+      <GridToolbarContainer
+        sx={{
+          width: 100,
+        }}
+      >
+        <GridToolbarExport />
+      </GridToolbarContainer>
+    );
+  }
   const columns: GridColDef[] = [
-    { field: "id", headerName: "ID", width: 70 },
-    { field: "name", headerName: "Name", width: 200 },
-    { field: "createdAt", headerName: "Created At", width: 200, type: "Date" },
+    { field: "id", headerName: "ID", width: 70, flex: 0.3 },
     {
-      field: "updatedAt",
-      headerName: "Updated At",
-      width: 150,
-      type: "Date",
+      field: "name",
+      headerName: "Name",
+      width: 300,
+      minWidth: 150,
+      flex: 0.3,
     },
-    { field: "weigth", headerName: "Weight", width: 200, type: "number" },
-    { field: "height", headerName: "Height", width: 200, type: "number" },
+    {
+      field: "weigth",
+      headerName: "Weight",
+      width: 150,
+      type: "number",
+      flex: 0.3,
+    },
+    {
+      field: "height",
+      headerName: "Height",
+      width: 150,
+      type: "number",
+      flex: 0.3,
+    },
+    {
+      field: "observations",
+      headerName: "Observations",
+      width: 250,
+      editable: true,
+      sortable: false,
+      flex: 0.3,
+    },
+    {
+      field: "available",
+      headerName: "Available",
+      width: 150,
+      editable: true,
+      sortable: false,
+      type: "boolean",
+      flex: 0.3,
+    },
   ];
 
   const rows = [];
@@ -37,19 +81,39 @@ const ArchivePage: React.FunctionComponent<IArchivePageProps> = (props) => {
   React.useEffect(() => {
     getAllData();
   }, []);
+  const processRowUpdate = (newRow: any) => {
+    const updatedRow = { ...newRow, isNew: false };
+    axios
+      .patch(`http://localhost:8888/player/${updatedRow.id}`, {
+        observations: updatedRow.observations,
+        available: updatedRow.available,
+      })
+      .then((response) => {
+        console.log(updatedRow);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    return updatedRow;
+  };
 
   for (let i = 0; i < data.length; i++) {
     rows[i] = data[i];
   }
   return (
     <Layout>
-      <Box
-        sx={{
-          height: 700,
-        }}
-      >
-        <DataGrid rows={rows} columns={columns} checkboxSelection={false} />
-      </Box>
+      <div style={{ height: 700, width: "100%" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          checkboxSelection={false}
+          processRowUpdate={processRowUpdate}
+          disableRowSelectionOnClick
+          slots={{
+            toolbar: CustomToolbar,
+          }}
+        />
+      </div>
     </Layout>
   );
 };
